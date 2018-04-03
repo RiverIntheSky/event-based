5433b4f197efadf67feba74c2ba65125e2c7805d
Weizhen Huang <weihuang@student.ethz.ch>
2018-04-03 20:51:16 +0200

    ignored /build

--- build/CMakeFiles/ev_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "ev_automoc.cpp"
-  "CMakeFiles/ev_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ev_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
