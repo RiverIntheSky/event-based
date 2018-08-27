20a9934c6db38aa0c711f172a9a85cecc28280f1
Weizhen Huang <weizhen@vpn-global-dhcp3-141.ethz.ch>
2018-08-27 04:49:18 +0200

    report: general scenes

--- Report/auto/report.el
+++ Report/auto/report.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "10pt" "twoside" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("packages/ethasl" "mt" "hs" "english")))
+                     '(("packages/ethasl" "mt" "hs" "english") ("algorithm2e" "ruled" "vlined")))
    (TeX-run-style-hooks
     "latex2e"
     "preambles/asl_preamble"
@@ -14,13 +14,13 @@
     "chapters/introduction"
     "chapters/chapter"
     "chapters/appendix_text"
-    "chapters/appendix_datasheets"
     "rep10"
     "pdfpages"
     "packages/ethasl"
     "mathrsfs"
     "algorithm2e"
-    "bm")
+    "bm"
+    "makecell")
    (LaTeX-add-bibliographies
     "bibliography/references"))
  :latex)
